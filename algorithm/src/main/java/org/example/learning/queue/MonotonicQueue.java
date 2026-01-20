4e0d6c52182a9d9c43fa5603efe485f910b3b7f0
Liu Yuanyi <gemini_lyy@163.com>
2026-01-20 10:43:17 +0800

    update monotonic queue

--- algorithm/src/main/java/org/example/learning/queue/MonotonicQueue.java
+++ algorithm/src/main/java/org/example/learning/queue/MonotonicQueue.java
@@ -11,33 +11,33 @@ public class MonotonicQueue<E extends Comparable<E>> {
     public void push(E e) {
         queue.offerLast(e);
 
-        while (!maxQueue.isEmpty() && maxQueue.getLast().compareTo(e) < 0) {
+        while (!maxQueue.isEmpty() && maxQueue.peekLast().compareTo(e) < 0) {
             maxQueue.pollLast();
         }
         maxQueue.offerLast(e);
 
-        while (!minQueue.isEmpty() && minQueue.getLast().compareTo(e) > 0) {
+        while (!minQueue.isEmpty() && minQueue.peekLast().compareTo(e) > 0) {
             minQueue.pollLast();
         }
         minQueue.offerLast(e);
     }
 
     public E max() {
-        return maxQueue.getFirst();
+        return maxQueue.peekFirst();
     }
 
     public E min() {
-        return minQueue.getFirst();
+        return minQueue.peekFirst();
     }
 
     public E pop() {
         E deleteValue = queue.pollFirst();
         assert deleteValue != null;
 
-        if (deleteValue.equals(maxQueue.getFirst())) {
+        if (deleteValue.equals(maxQueue.peekFirst())) {
             maxQueue.pollFirst();
         }
-        if (deleteValue.equals(minQueue.getFirst())) {
+        if (deleteValue.equals(minQueue.peekFirst())) {
             minQueue.pollFirst();
         }
         return deleteValue;
